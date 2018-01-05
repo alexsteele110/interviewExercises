c52cef1529c1144528c75a3065f063bdfb9e3881
Alex Steele <alexsteele110@gmail.com>
2018-01-05 13:11:21 -0500

    several exercises completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,39 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// function reverse(str) {
+//   const splitStr = str.split('');
+//   const reversed = [];
+//
+//   for (var i = splitStr.length - 1; i >= 0; i--) {
+//     reversed.push(splitStr[i]);
+//   }
+//   return reversed.join('');
+// }
+
+// function reverse(str) {
+//   let reversed = '';
+//
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//
+//   return reversed;
+// }
+
+// function reverse(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '');
+// }
