ab60e9a0cfa75cb95a4f6374fafc903be7ef0ce0
Alex Steele <alexsteele110@gmail.com>
2018-01-08 18:57:19 -0500

    more exercises

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,62 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
 
 module.exports = fib;
+
+// function fib(n) {
+//   const sequence = [0, 1];
+//
+//   if (n > 1) {
+//     for (let i = 2; i <= n; i++) {
+//       sequence.push(sequence[i - 2] + sequence[i - 1]);
+//     }
+//     return sequence[n];
+//   } else {
+//     return sequence[n];
+//   }
+// }
+
+// function fib(n) {
+//   const result = [0, 1];
+//
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//
+//     result.push(a + b);
+//   }
+//
+//   return result[n];
+// }
+
+// function fib(n, result = [0, 1], index = 2) {
+//   if (n < index) {
+//     return result[n];
+//   }
+//
+//   result.push(result[index - 1] + result[index - 2]);
+//   index++;
+//   return fib(n, result, index);
+// }
--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,59 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  if (n - 1 - row <= level.length && n - 1 + row >= level.length) {
+    level += '#';
+  } else {
+    level += ' ';
+  }
+  pyramid(n, row, level);
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log(
+//       constructPieces(n - i, ' ') +
+//         constructPieces(2 * i - 1, '#') +
+//         constructPieces(n - i, ' ')
+//     );
+//   }
+// }
+//
+// function constructPieces(n, symbol) {
+//   let pieces = '';
+//
+//   for (let i = 0; i < n; i++) {
+//     pieces += symbol;
+//   }
+//
+//   return pieces;
+// }
+
+// function pyramid(n) {
+//   const midpoint = n - 1;
+//
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
+//
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
