ed983455abb6a05b370454e92b767edeb5c6cdfa
Alexander Steele <alexsteele110@gmail.com>
2018-01-10 22:40:08 -0500

    more exercises complete

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,45 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const arr = [root, 's'];
+  const counters = [0];
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === 's') {
+      counters.push(0);
+      arr.push('s');
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
+
+// function levelWidth(root) {
+//   const results = [1];
+//   let arr = [root];
+//   let temp = [];
+//
+//   while (arr.length) {
+//     for (let node of arr) {
+//       temp.push(...node.children);
+//     }
+//
+//     if (temp.length) {
+//       results.push(temp.length);
+//       arr = temp;
+//       temp = [];
+//     } else {
+//       return results;
+//     }
+//   }
+//
+//   return results;
+// }
